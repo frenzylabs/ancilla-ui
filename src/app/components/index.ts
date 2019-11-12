4320282567c88296562486d7498ee82d37247bf8
Kevin Musselman <kmussel@gmail.com>
2019-11-12 15:41:19 -0500

    remove unneeded

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -8,6 +8,4 @@
 
 export {default as Nav}       from './nav'
 export {default as SubNav}    from './subnav'
-export {default as Statusbar} from './statusbar'
-export {default as Summary}   from './summary'
-export {default as Terminal}  from './terminal'
+
